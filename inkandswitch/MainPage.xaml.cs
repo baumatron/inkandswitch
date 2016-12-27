4a59ac13bf762764437d77f338d7868e0df966ca
Brian Baumhover <b.baumhover@gmail.com>
2016-12-27 15:03:55 -0600

    Add input handlers to page. Handle ctrl-z to undo by erasing the last stroke in the canvas. A key to getting this to work was adding IsTabStop=“true” to the root Page element so the framework will invoke KeyDown and KeyUp events. If there were a view element in the scene at a lower level that accepts keyboard input, the events would get bubbled up, but since that isn’t the case, IsTabStop must be set to true.
    Add additional InkCanvas instance to the page. Create a Grid layout with columns that expand to fill the width of the page.
    
    Naively propagate strokes on completion from each canvas to the other.

--- inkandswitch/MainPage.xaml.cs
+++ inkandswitch/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Collections.Generic;
+using Windows.System;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Linq;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -13,11 +19,75 @@ namespace inkandswitch
         {
             this.InitializeComponent();
 
-            this.InkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse;
-            this.InkCanvas.InkPresenter.InputDeviceTypes =
+            this.Loaded += OnLoaded;
+
+            ConfigureCanvasInput(this.LeftCanvas);
+            ConfigureCanvasInput(this.RightCanvas);
+
+            LeftCanvas.InkPresenter.StrokesCollected += (sender, args) => ApplyStrokesToCanvas(RightCanvas, args);
+            RightCanvas.InkPresenter.StrokesCollected += (sender, args) => ApplyStrokesToCanvas(LeftCanvas, args);
+        }
+
+        private void ApplyStrokesToCanvas(InkCanvas destinationCanvas, InkStrokesCollectedEventArgs args)
+        {
+            // Need to do a deep copy of the strokes, as one stroke can't exist in two separate canvases
+            foreach (var stroke in args.Strokes)
+            {
+                destinationCanvas.InkPresenter.StrokeContainer.AddStroke(stroke.Clone());
+            }
+        }
+
+        private void ConfigureCanvasInput(InkCanvas canvas)
+        {
+            canvas.InkPresenter.InputDeviceTypes =
                 Windows.UI.Core.CoreInputDeviceTypes.Mouse |
                 Windows.UI.Core.CoreInputDeviceTypes.Pen |
                 Windows.UI.Core.CoreInputDeviceTypes.Touch;
         }
+        
+        private void OnLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Give input focus to the entire page
+            this.Focus(Windows.UI.Xaml.FocusState.Programmatic);
+        }
+
+        private void OnKeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("OnKeyUp");
+            if (VirtualKey.Control == e.Key)
+            {
+                isCtrlPressed = false;
+            }
+        }
+
+        private void OnKeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("OnKeyDown");
+            if (VirtualKey.Control == e.Key)
+            {
+                isCtrlPressed = true;
+            }
+            else if (isCtrlPressed)
+            {
+                if (VirtualKey.Z == e.Key)
+                {
+                    Undo(LeftCanvas);
+                }
+            }
+        }
+
+        private void Undo(InkCanvas canvas)
+        {
+            System.Diagnostics.Debug.WriteLine("Undo!");
+
+            IReadOnlyList<InkStroke> strokes = canvas.InkPresenter.StrokeContainer.GetStrokes();
+            if (strokes.Count > 0)
+            {
+                strokes.Last().Selected = true;
+                canvas.InkPresenter.StrokeContainer.DeleteSelected();
+            }
+        }
+
+        private bool isCtrlPressed = false;
     }
 }
