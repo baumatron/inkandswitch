aca79651c3d7f61edf64c9a7953cf305a66cc3f4
Brian Baumhover <b.baumhover@gmail.com>
2016-12-27 14:00:13 -0600

    Add additional input types for inking.

--- inkandswitch/MainPage.xaml.cs
+++ inkandswitch/MainPage.xaml.cs
@@ -14,6 +14,10 @@ namespace inkandswitch
             this.InitializeComponent();
 
             this.InkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse;
+            this.InkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen |
+                Windows.UI.Core.CoreInputDeviceTypes.Touch;
         }
     }
 }
